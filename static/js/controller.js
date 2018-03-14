f0d00d388a652a26f66e929f74d152ad96673a0e
Jeff <AlexBliss24@users.noreply.github.com>
2018-03-14 11:47:01 +0000

    Add files via upload

--- static/js/controller.js
+++ static/js/controller.js
@@ -22,9 +22,12 @@ app.controller("tController", function($scope, $interval, $sce, $http){
 	//
 	
 	$scope.loadModal = function(path,name){
-		$http.get(path).then(function(res){
-			$scope.modals[name] = $sce.trustAsHtml(res.data);
-		});
+		if (!$scope.modals[name])
+		{
+			$http.get(path).then(function(res){
+				$scope.modals[name] = $sce.trustAsHtml(res.data);
+			});
+		}
 	};
 	$scope.showModal = function(modal){
 		$scope.modalContent = $scope.modals[modal];
@@ -34,7 +37,6 @@ app.controller("tController", function($scope, $interval, $sce, $http){
 	$scope.modals = {};
 	$scope.loadModal("./static/pages/modal/login.html", "login");
 	$scope.loadModal("./static/pages/modal/markov.html", "markov");
-	$scope.loadModal("./static/pages/modal/random.html", "random");
 	//
 	
 });
